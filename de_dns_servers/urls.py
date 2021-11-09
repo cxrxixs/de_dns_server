add48c7cb1dbbc0f57091b983c55fad7724bb290
Susan Sim <s.sim@motainment.de>
2021-11-09 15:34:09 +0800

    Update api URL to match task files

--- de_dns_servers/urls.py
+++ de_dns_servers/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('filter/', views.filter_de_dns_server, name='filter_de_dns_server'),
-    path('api/dns', views.api_de_dns_server, name='api_de_dns_server'),
+    path('api/dns-server', views.api_de_dns_server, name='api_de_dns_server'),
 ]
